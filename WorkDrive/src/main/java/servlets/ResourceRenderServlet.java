4c1087ef6fe53e18e459a85ab4907bc09ec2849d
krishnakumarprabha-ms <krishnakumarprabha2008@gmail.com>
2026-02-01 17:26:43 +0530

    resourceRendering update

--- WorkDrive/src/main/java/servlets/ResourceRenderServlet.java
+++ WorkDrive/src/main/java/servlets/ResourceRenderServlet.java
@@ -16,64 +16,76 @@ import dao.AccountsManager;
 import dao.ResourceManager;
 
 /**
-* Servlet implementation class ShowFiles
-*/
+ * Servlet implementation class ShowFiles
+ */
 public class ResourceRenderServlet extends HttpServlet {
-   private static final long serialVersionUID = 1L;
-     
-   /**
-    * @see HttpServlet#HttpServlet()
-    */
-   public ResourceRenderServlet() {
-       super();
-       // TODO Auto-generated constructor stub
-   }
+    private static final long serialVersionUID = 1L;
 
-   /**
-    * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-    */
-   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-       // TODO Auto-generated method stub
-       response.getWriter().append("Served at: ").append(request.getContextPath());
-   }
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ResourceRenderServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
-   /**
-    * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-    */
-   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-       // TODO Auto-generated method stub
-       try {
-           JSONObject requestObject = new JSONObject(RequestHandler.getRequestObjectString(request));
-           Long parentId = null;
-           long userId = 0;
-           Cookie[] cookies = request.getCookies();
-           String cookieValue = null;
-           if (cookies != null) {
-               for (Cookie cookie : cookies) {
-                   if ("cookie".equals(cookie.getName())) {
-                       cookieValue = cookie.getValue();
-                       break;
-                   }
-               }
-               userId = AccountsManager.getUserId(cookieValue);
-           }
-           if (!requestObject.isNull("parentId")) {
-               parentId = requestObject.getLong("parentId");
-           } else {
-               parentId = ResourceManager.getMyFolderId(userId);
-           }
-           ArrayList<JSONObject> folders = ResourceManager.getResource(parentId, userId);
-           ArrayList<JSONObject> files = ResourceManager.getAllFiles(parentId);
-           ArrayList<JSONObject> resources = new ArrayList<>();
-           resources.addAll(folders);
-           resources.addAll(files);
-           response.setContentType("application/json");
-           response.setCharacterEncoding("UTF-8");
-           response.getWriter().write(RequestHandler.sendResponse(200, "Resources rendered successfully", resources));
-       } catch (Exception e) {
-           e.printStackTrace();
-           response.getWriter().write(RequestHandler.sendResponse(500, "Failed to render resource"));
-       }
-   }
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        response.getWriter().append("Served at: ").append(request.getContextPath());
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        try {
+            JSONObject requestObject = new JSONObject(RequestHandler.getRequestObjectString(request));
+            Long parentId = null;
+            long userId = 0;
+            Cookie[] cookies = request.getCookies();
+            if (cookies != null) {
+                for (Cookie cookie : cookies) {
+                    if ("cookie".equals(cookie.getName())) {
+                        userId = AccountsManager.getUserId(cookie.getValue());
+                        break;
+                    }
+                }
+            }
+            if (!requestObject.isNull("parentId")) {
+                parentId = requestObject.getLong("parentId");
+            } else {
+                parentId = ResourceManager.getMyFolderId(userId);
+            }
+
+            ArrayList<JSONObject> folders = ResourceManager.getResource(parentId, userId);
+            ArrayList<JSONObject> files = ResourceManager.getAllFiles(parentId);
+            ArrayList<JSONObject> resources = new ArrayList<>();
+            for (JSONObject folder : folders) {
+                folder.put("type", "FOLDER");
+                folder.put("id", folder.getLong("resourceId"));
+                resources.add(folder);
+            }
+            for (JSONObject file : files) {
+                file.put("type", "FILE");
+                file.put("id", parentId + "-" + file.getString("filename"));
+                resources.add(file);
+            }
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write(RequestHandler.sendResponse(200, "Resources rendered successfully", resources));
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.getWriter().write(RequestHandler.sendResponse(500, "Failed to render resource"));
+        }
+    }
+>>>>>>> d17f30f (resourceRendering update)
 
 }
